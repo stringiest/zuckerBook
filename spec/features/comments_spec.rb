dadeddd6dd4099f77efe0ee34a24f09b59372d64
Malachi Spencer <malachispencer@MA309.fritz.box>
2021-01-07 15:41:20 +0000

    Comment on post page feature test passing

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -1,12 +1,19 @@
 feature 'comments' do
-  xscenario 'user can comment on post' do
+  before(:all) do
+    Capybara.current_driver = :selenium
+  end
+
+  scenario 'user can comment on post on individual post' do
     login_and_make_post
-    click_link('Commentsx')
-    p current_path
-    fill_in('body', with: 'It sure is!')
+    click_link('comment-link')
+    fill_in('comment_body', with: 'It sure is!')
     click_button 'Comment'
 
     expect(current_path).to eq('/posts/1')
     expect(page).to have_content('It sure is!')
+  end
+
+  after(:all) do
+    Capybara.use_default_driver
   end
 end

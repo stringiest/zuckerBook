0803378de4ec850eeb3abbaad2b9679ebbd7256d
Malachi Spencer <malachispencer@MA309.fritz.box>
2021-01-07 14:49:23 +0000

    Post feature tests passing

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -1,24 +1,24 @@
 feature 'posts' do
-  xscenario 'user can sign in and make a post' do
+  scenario 'user can sign in and make a post' do
     login_and_make_post
 
     expect(current_path).to eq('/posts')
     expect(page).to have_content('2021 will be a good year')
   end
 
-  xscenario 'user can edit a post' do
+  scenario 'user can edit a post' do
     login_and_make_post
     click_link('Edit')
     fill_in('post_body', with: '')
     fill_in('post_body', with: '2020 sucked')
 
-    click_button('Update Post')
+    click_button('Submit')
 
     expect(current_path).to eq('/posts')
     expect(page).to have_content('2020 sucked')
   end
 
-  xscenario 'user can delete a post' do
+  scenario 'user can delete a post' do
     login_and_make_post
     click_link('Delete')
     click_link('ok')
